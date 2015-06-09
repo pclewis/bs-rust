791481b5a9307ebd74b5912b549c9e1c32c68062
Philip Lewis <pcl@pclewis.com>
2015-06-09 14:35:04 -0400

    union and intersect for judy, simple bench2 test

--- src/bitset/judy.rs
+++ src/bitset/judy.rs
@@ -22,18 +22,54 @@ impl Bitset<String> for JudyBitset
         }
         return Ok( () );
     }
+
     fn union(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, String >
     {
-        unimplemented!();
+        let mut result = Judy1::new();
+        for input in keys.iter().filter_map( |k| self.sets.get(&format!("{}-{}", set_name, k)) ) {
+            for v in input.iter() {
+                result.set( v );
+            }
+        }
+        return Ok( result.iter().map(|it|it as usize).collect() );
     }
+
     fn intersect(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, String >
     {
-        unimplemented!();
+        let mut result = Judy1::new();
+        let mut inputs : Vec<&Judy1> = keys.iter().filter_map( |k| self.sets.get(&format!("{}-{}", set_name, k)) ).collect();
+        if inputs.len() < keys.len() {
+            return Ok( vec![0] )
+        }
+
+        inputs.sort_by( |a,b| a.len().cmp(&b.len()) );
+        let mut iters : Vec<_> = inputs.iter().map( |i| i.iter() ).collect();
+
+        'outer: loop {
+            match iters[0].next() {
+                None => break,
+                Some(tgt) => {
+                    'iters: for iter in iters.iter_mut().skip(1) {
+                        'inner: loop {
+                            match iter.next() {
+                                Some(n) if n == tgt => continue,
+                                Some(n) if n > tgt  => break 'iters,
+                                Some(_)             => break 'inner,
+                                None                => break 'outer
+                            }
+                        }
+                        result.set(tgt);
+                    }
+                }
+            }
+
+        }
+        return Ok( result.iter().map(|it|it as usize).collect() );
     }
 }
 
 impl JudyBitset {
-    fn new() -> JudyBitset
+    pub fn new() -> JudyBitset
     {
         return JudyBitset{ sets: HashMap::new() };
     }
