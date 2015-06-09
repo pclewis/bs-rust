277e19ad7ae8f7e5b31459ecbdaa8e2dd1d5064f
Philip Lewis <pcl@pclewis.com>
2015-06-09 15:43:11 -0400

    list method on bitsets, some refactoring

--- src/bitset/mod.rs
+++ src/bitset/mod.rs
@@ -3,6 +3,7 @@ pub trait Bitset<T>
     fn add(&mut self, set_name: &str, key: usize, values: &[usize]) -> Result< (), T >;
     fn union(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, T >;
     fn intersect(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, T >;
+    fn list(&mut self, set_name: &str, key: usize) -> Result< Vec<usize>, T >;
 }
 
 
--- src/bitset/redis.rs
+++ src/bitset/redis.rs
@@ -6,28 +6,36 @@ pub struct RedisBitset<'a>
     conn: & 'a Connection,
 }
 
+fn key_str(set_name: &str, key: usize) -> String
+{
+    return format!("{}-{}", set_name, key);
+}
+
+fn key_strs(set_name: &str, keys: &[usize]) -> Vec<String>
+{
+    return keys.iter().map( |k| format!("{}-{}", set_name, k) ).collect();
+}
+
 impl<'a> Bitset<RedisError> for RedisBitset<'a>
 {
     fn add(&mut self, set_name: &str, key: usize, values: &[usize]) -> RedisResult< () >
     {
-        let key_str = format!("{}-{}", set_name, key);
-        // need let to make types explicit
-        let res : RedisResult<()> = self.conn.sadd(key_str, values);
-        res
+        return self.conn.sadd(key_str(set_name,key), values);
     }
 
     fn union(&mut self, set_name: &str, keys: &[usize]) -> RedisResult< Vec<usize> >
     {
-        let key_strs : Vec<String> = keys.iter().map( |k| format!("{}-{}", set_name, k) ).collect();
-        let res : RedisResult<Vec<usize>> = self.conn.sunion( key_strs );
-        res
+        return self.conn.sunion( key_strs(set_name, keys) );
     }
 
     fn intersect(&mut self, set_name: &str, keys: &[usize]) -> RedisResult< Vec<usize> >
     {
-        let key_strs : Vec<String> = keys.iter().map( |k| format!("{}-{}", set_name, k) ).collect();
-        let res : RedisResult<Vec<usize>> = self.conn.sinter( key_strs );
-        res
+        return self.conn.sinter( key_strs(set_name, keys) );
+    }
+
+    fn list(&mut self, set_name: &str, key: usize) -> RedisResult<Vec<usize>>
+    {
+        return self.conn.smembers( key_str(set_name, key) );
     }
 }
 
