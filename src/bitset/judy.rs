d4542968adc45c7280492bcb5e7e38dc18ecb245
Philip Lewis <pcl@pclewis.com>
2015-06-09 15:43:11 -0400

    list method on bitsets, some refactoring

--- src/bitset/judy.rs
+++ src/bitset/judy.rs
@@ -8,11 +8,21 @@ pub struct JudyBitset
     sets : HashMap< String, Judy1 >,
 }
 
+fn key_str(set_name: &str, key: usize) -> String
+{
+    return format!("{}-{}", set_name, key);
+}
+
+fn key_strs(set_name: &str, keys: &[usize]) -> Vec<String>
+{
+    return keys.iter().map( |k| format!("{}-{}", set_name, k) ).collect();
+}
+
 impl Bitset<String> for JudyBitset
 {
     fn add(&mut self, set_name: &str, key: usize, values: &[usize]) -> Result< (), String >
     {
-        let key_name = format!("{}-{}", set_name, key);
+        let key_name = key_str(set_name, key);
         if !self.sets.contains_key(&key_name) {
             self.sets.insert( key_name.to_owned(), Judy1::new() );
         }
@@ -26,7 +36,7 @@ impl Bitset<String> for JudyBitset
     fn union(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, String >
     {
         let mut result = Judy1::new();
-        for input in keys.iter().filter_map( |k| self.sets.get(&format!("{}-{}", set_name, k)) ) {
+        for input in key_strs(set_name, keys).iter().filter_map( |k| self.sets.get(k) ) {
             for v in input.iter() {
                 result.set( v );
             }
@@ -37,7 +47,12 @@ impl Bitset<String> for JudyBitset
     fn intersect(&mut self, set_name: &str, keys: &[usize]) -> Result< Vec<usize>, String >
     {
         let mut result = Judy1::new();
-        let mut inputs : Vec<&Judy1> = keys.iter().filter_map( |k| self.sets.get(&format!("{}-{}", set_name, k)) ).collect();
+        let mut inputs : Vec<&Judy1> =
+            key_strs(set_name, keys)
+            .iter()
+            .filter_map( |k| self.sets.get(k) )
+            .collect();
+
         if inputs.len() < keys.len() {
             return Ok( vec![0] )
         }
@@ -65,6 +80,13 @@ impl Bitset<String> for JudyBitset
 
         }
         return Ok( result.iter().map(|it|it as usize).collect() );
+    }
+
+    fn list(&mut self, set_name: &str, key: usize) -> Result< Vec<usize>, String >
+    {
+        let key_name = format!("{}-{}", set_name, key);
+        let j = self.sets.get_mut(&key_name).unwrap();
+        return Ok( j.iter().map(|i| i as usize ).collect() );
     }
 }
 
